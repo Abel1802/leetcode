8f3fb38f15aaaf30a9a90d466d42e79bb0b58c92
Abel1802 <lzblcu19@gmail.com>
2021-04-10 11:03:59 +0800

    addTwoNumbers

--- 2.py
+++ 2.py
@@ -1,110 +1,48 @@
 #!/usr/bin/env python3
 # author fool; date 2020-02-29
-'''
-给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
 
-如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
 
-您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
 
-示例：
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
-输出：7 -> 0 -> 8
-原因：342 + 465 = 807
 
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/add-two-numbers
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-'''
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        '''
+            第一：首先逆序不知如何解决
+                其实，先逆序求和，得到结果后在逆序回来和一开始正序求和的结果一样！！！
 
-# python list version （the entitle request ListNode type）
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        l3 = []
-        len1 = len(l1)
-        len2 = len(l2)
-        # list1 与list2 等长
-        if len1 == len2:
-            # 记录该位是否逢十进一
-            flag = 0
-            for i in range(len1):
-                tmp = l1[i] + l2[i]
-                if tmp > 9:
-                    tmp = tmp - 10
-                    l3.append(tmp + flag)
-                    flag = 1
-                else:
-                    l3.append(tmp + flag)
-                    flag = 0
-            # 最后一位是否进一
-            if flag == 1:
-                l3.append(1)
-        # list1 长度小于 list2
-        elif len1 < len2:
-            flag = 0
-            index = 0
-            # 在list1长度和list2相等的情况
-            for i in range(len1):
-                index += 1
-                tmp = l1[i] + l2[i]
-                if tmp > 9:
-                    tmp = tmp - 10
-                    l3.append(tmp + flag)
-                    flag = 1
-                else:
-                    l3.append(tmp + flag)
-                    flag = 0
-            # 在list2剩余的长度下的情况
-            for j in range(index, len2):
-                tmp = l2[j] + flag
-                if tmp > 9:
-                    tmp = tmp - 10
-                    l3.append(tmp )
-                    flag = 1
-                else:
-                    l3.append(tmp)
-                    flag = 0
-            # 最后一位是否进一
-            if flag == 1:
-                l3.append(1)
-        # list1 长度大于 list2
-        elif len1 > len2:
-            flag = 0
-            index = 0
-            # 在list1长度和list2相等的情况
-            for i in range(len2):
-                index += 1
-                tmp = l1[i] + l2[i]
-                if tmp > 9:
-                    tmp = tmp - 10
-                    l3.append(tmp + flag)
-                    flag = 1
-                else:
-                    l3.append(tmp + flag)
-                    flag = 0
-            # 在list1剩下的长度情况
-            for j in range(index, len1):
-                tmp = l1[j] + flag
-                if tmp > 9:
-                    tmp = tmp - 10
-                    l3.append(tmp)
-                    flag = 1
-                else:
-                    l3.append(tmp)
-                    flag = 0
-            # 最后一位是否进一
-            if flag == 1:
-                l3.append(1)
-        return l3
+            第二：两个列表的遍历有问题
+                先更新next
+                再更新val
+        '''
+        carry = 0
+        ret = ListNode()
+        cur = ret
+        while l1 or l2 or carry:
+            num = 0
+            if l1:
+                num += l1.val
+                l1 = l1.next
+            if l2:
+                num += l2.val
+                l2 = l2.next
+            if carry:
+                num += carry
+                carry -= 1
+            carry, num = divmod(num, 10)
+            cur.next = ListNode(num)
+            cur = cur.next
+
+        return ret.next
 
 if __name__ == '__main__':
-    print('Using python list type!')
-    solution = Solution()
-    print(solution.addTwoNumbers([9, 8, 9], [2, 5]))
-
+    
+    a = 12
+    
+    b, c = divmod(a, 10)
+    print(b, c)
