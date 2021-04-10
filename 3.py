a9d8654576b9c8014bff14e1a45ae4eb37142071
Abel1802 <lzblcu19@gmail.com>
2021-04-10 13:22:18 +0800

    hash

--- 3.py
+++ 3.py
@@ -2,55 +2,33 @@
 # author fool
 # date 2020-03-01
 '''
-给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
-
-示例 1:
-
-输入: "abcabcbb"
-输出: 3
-解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+    Leetcode-3 无重复字符的最长子串
 '''
-# using a violent method
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        # length of string s
-        L = len(s)
-        max_sub = 0
-        if L == 1:
-            return max_sub + 1
-        else:
-            for i in range(L):
-                # 因为这里是取当前子串 所以是 i to L+1
-                for j in range(i,L+1):
-                    # 取当前子串
-                    tmp = s[i:j]
-                    #print('tmp: {}; i: {}; j: {}'.format(tmp, i, j))
-                    # 计算当前子串中不重复的字符个数
-                    counts = {}
-                    for str in tmp:
-                        counts[str] = counts.get(str, 0 ) + 1
-                    if len(counts) != len(tmp):
-                        continue
-                    else:
-                        if len(tmp) > max_sub:
-                            max_sub = len(tmp)
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        '''
+            想到hash，但是不知道怎么控制, 例如 " a b c a b c b b "
 
-            return max_sub
+            难点一：找最大子串，所以一定会有两个关键值：
+                                                第一个都容易想到，就是遍历原字符串；（其实是子串的末尾下标）
+                                                第二个容易忽略，控制子串的起始起始下标（用一个变量控制）
 
-
-if __name__ == '__main__':
-    solution = Solution()
-    print(solution.lengthOfLongestSubstring('abcabcbb'))
-    print(solution.lengthOfLongestSubstring('au'))
-    print(solution.lengthOfLongestSubstring(' '))
-    print(solution.lengthOfLongestSubstring(''))
-
-
+            难点二：如何更新子串的起始下标k: 用hash记录每个字母c的下标：
+                                        if c in dict 且 上一次的c的下标 > 子串起始下标k： (保证了在k前面的字符的重复也被算进去) 例如"tmmzuxt"
+                                            更新k, 
+                                            更新hash, 
+                                        else:
+                                            dict[c] = i
+                                            res = max(res, i-k)
+        '''
+        res, c_dict = 0, {}
+        k = -1 # 用于控制子串的起始点（从-1的后面开始，也就是i - k的子串长度， i是末尾点）
+        for i, c in enumerate(s):
+            if c in c_dict and c_dict[c] > k:  # 字符c在字典中 且 上次出现的下标大于当前长度的起始下标
+                k = c_dict[c]
+                c_dict[c] = i
+            else:
+                c_dict[c] = i
+                res = max(res, i-k)
+        return res
+        
